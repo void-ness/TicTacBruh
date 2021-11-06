eb2a0333251687fa347a09dd4e6b30eaec814e0a
void-ness <78547746+void-ness@users.noreply.github.com>
2021-11-06 19:06:10 +0530

    Added: Tie Feature

--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@
 let Gamebox = document.getElementsByClassName("Box");
 let turn = "X";
 let GameOver = false;
+let Tie = false;
 let ColorsList = [1,2,3,4,5,6,7,8,9,0,"A","B","C","D","E","F"];
 let Color="#";
 // screenSize matches atrribute will become true when the screen size matches
@@ -29,16 +30,37 @@ function CheckScreen(x) {
 //winning Logic
 function Winyet(){
     let win = [ [0,1,2,5,5,0] , [3,4,5,5,15,0] , [6,7,8,5,25,0] , [0,3,6,-5,15,90] , [1,4,7,5,15,90] , [2,5,8,15,15,90] , [0,4,8,5,15,45] , [2,4,6,5,15,135] ];
-
+    
+    // this checks for each of the winning combination
     win.forEach( (WinComb) => {
         if (Gamebox[WinComb[0]].innerText === Gamebox[WinComb[1]].innerText & Gamebox[WinComb[0]].innerText === Gamebox[WinComb[2]].innerText && Gamebox[WinComb[0]].innerText != "" ){
             GameOver = true;
             PointsCalc(Gamebox[WinComb[0]].innerText);
             document.getElementsByClassName("WinLine")[0].style.width = "20vw";
             document.getElementsByClassName("WinLine")[0].style.transform = `translate(${WinComb[3]}vw , ${WinComb[4]}vw) rotate(${WinComb[5]}deg) scaleX(1.3)`;
-            return 0;
         }
     })
+
+    // this code below is for checking for a Tie
+
+    // after no one has won yet, the true value is changed to true
+    Tie = true;
+
+    // however if there is any empty box the Tie value is changed to False
+    Array.from(Gamebox).forEach( (box) => {
+        if (box.querySelector(".boxtext").innerText == ""){
+            Tie = false;
+        } 
+    })
+
+    // this gets processed when the No one has won yet, neither any box is left empty
+    if (Tie){
+        GameOver = true;
+        PointsCalc("XO");
+        return 0;
+    }
+
+    // console.log("alpha male")
 }
 
 // pointer logic
@@ -51,27 +73,39 @@ function PointsCalc(turn){
     else if (turn == "O"){
         WinO += 1;
     }
+
+    else if (turn == "XO"){
+        WinX += 1;
+        WinO += 1;
+    }
 }
 
 // main logic
 Array.from(Gamebox).forEach( (box) => {
     box.addEventListener("click",() => {
-        let text = box.querySelector(".boxtext");
-        if (text.innerText === ""){
-            text.innerText = turn;
-            Winyet();
+        if (!GameOver){
+            let text = box.querySelector(".boxtext");
+            if (text.innerText === ""){
+                text.innerText = turn;
+                Winyet();
 
-            if (GameOver & turn != ""){
-                document.querySelector(".TurnInfo").innerText = `Congratulations ${turn} Won! yayy`;
-                CheckScreen(screenSize);
-                document.querySelector(".PointsX").innerText = WinX;
-                document.querySelector(".PointsO").innerText = WinO;
-                turn = ""
-            }
+                if (GameOver){
+                    if (Tie){
+                        document.querySelector(".TurnInfo").innerText = "Tough Match! It's a Tie.";
+                    }
+                    else if (!Tie){
+                        document.querySelector(".TurnInfo").innerText = `Congratulations ${turn} Won! yayy`;
+                    }
 
-            if (!GameOver){
-                turn = TurnChanger();
-                document.querySelector(".TurnInfo").innerText = `Turn for ${turn} Now`;
+                    CheckScreen(screenSize);
+                    document.querySelector(".PointsX").innerText = WinX;
+                    document.querySelector(".PointsO").innerText = WinO;
+                }
+
+                if (!GameOver){
+                    turn = TurnChanger();
+                    document.querySelector(".TurnInfo").innerText = `Turn for ${turn} Now`;
+                }
             }
         }
     })
@@ -79,9 +113,11 @@ Array.from(Gamebox).forEach( (box) => {
 
 // reset logic
 document.getElementById("GameReset").addEventListener("click", function(){
+    // makes all the boxes again empty
     Array.from(Gamebox).forEach( (box) => {
         box.querySelector(".boxtext").innerText = "";
     })
+
     // as games begin with turn for X first
     turn = "X";
     GameOver = false;
@@ -90,17 +126,14 @@ document.getElementById("GameReset").addEventListener("click", function(){
     //hiding the line that appears after winning
     document.getElementsByClassName("WinLine")[0].style.width = "0";
 
-    //works like a button to reset color too as I prefer white bg only
-    document.getElementsByTagName("body")[0].style.background = "white";
-
     // Resets the Win Pop Ups
     document.querySelector(".WinPopUpImg").style.transform = "translateY(19em)";
     document.querySelector(".congratulate").style.height = "0";        
 })
 
 // color changer logic
-
 document.getElementById("ColorChanger").addEventListener("click", () =>{
+    let i;
     for (i = 0; i <6; i++){
         // this gets me a random number b/w 0 to 15 as color list array has 16 elements
         let random = Math.floor(Math.random(1)*16);
